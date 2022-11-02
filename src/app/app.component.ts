2068fb5db3b863cc2df03daae43947055c88cc33
nolto1sj <sjnolton@gmail.com>
2022-11-02 15:23:44 -0400

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,53 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-todo';
+
+  appInput: string = "";
+
+  todoArray: Todo[] = [
+    {
+      task: "Fold Laundry",
+      completed: false
+    },
+    {
+      task: "Clean Desk",
+      completed: false
+    },
+    {
+      task: "Feed Gecko",
+      completed: true
+    },
+    {
+      task: "Feed Cat",
+      completed: true
+    },
+    {
+      task: "Feed Self",
+      completed: false
+    },
+    {
+      task: "Call State Farm",
+      completed: false
+    },
+    {
+      task: "Do Homework",
+      completed: true
+    }
+  ];
+
+  returnsTodo(): Todo[] {
+    return this.todoArray;
+  }
+  addTask(taskInput: NgForm): void {
+    let newTask: string = taskInput.form.value.input;
+    let todo: Todo = {
+      task: newTask,
+      completed: false,
+    };
+    this.todoArray.push(todo);
+  }
+
+  completeTask(todo: Todo): void {
+    todo.completed = true;
+  }
 }
